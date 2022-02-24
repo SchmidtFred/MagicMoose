7e4329c9fb8b4dbfee5a9f0d0541c8ff613f06b9
SchmidtFred <fschmidt@live.com>
2022-02-24 13:53:26 -0600

    finished magic moose

--- Program.cs
+++ Program.cs
@@ -1,12 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace MagicMoose
+Random randomizer = new Random();
+List<string> responseList = new List<string>();
+
+Console.WriteLine("Welcome to the Enthusiastic Magic Moose Simulator!");
+Console.WriteLine("--------------------------------------------");
+IntializeList();
+AskMoose();
+
+
+void MooseSays(string message)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    Console.WriteLine($@"
+                                      _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    {message}
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+    ");
+}
+
+void IntializeList() {
+    responseList.Add("As I see it, yes.");
+    responseList.Add("Ask again later.");
+    responseList.Add("Better not tell you now.");
+    responseList.Add("Cannot predict now.");
+    responseList.Add("Concentrate and ask again.");
+    responseList.Add("Don't count on it.");
+    responseList.Add("It is certain.");
+    responseList.Add("It is decidedly so.");
+    responseList.Add("Most likely.");
+    responseList.Add("My reply is no.");
+    responseList.Add("My sources say no.");
+    responseList.Add("Outlook not so good.");
+    responseList.Add("Outlook good.");
+    responseList.Add("Reply hazy, try again.");
+    responseList.Add("Signs point to yes.");
+    responseList.Add("Very doubtful.");
+    responseList.Add("Without a doubt.");
+    responseList.Add("Yes.");
+    responseList.Add("Yes-definitely.");
+    responseList.Add("You may rely on it.");
+}
+
+void AskMoose() {
+    MooseSays("Ask me anything, I know all.");
+    Console.ReadLine();
+
+    int randomNumber = randomizer.Next(20);
+    MooseSays(responseList[randomNumber]);
 }
